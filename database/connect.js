8838733431598bfb9105c923696e43710ba20fb3
saroyar <yarsaro2001@gmail.com>
2023-03-04 19:26:43 +0530

    test f

--- database/connect.js
+++ database/connect.js
@@ -1,6 +1,7 @@
 const mongoose=require('mongoose');
 
 exports.connect=async()=>{
+    mongoose.set('strictQuery', true);
     mongoose.connect(process.env.DatabaseUrl)
     .then((con)=>console.log(`database connected at ${con.connection.host}`))
     .catch((err)=>console.log(err));
