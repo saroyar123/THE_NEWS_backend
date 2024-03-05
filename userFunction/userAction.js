7fc95db4c02c161424ea977326d016937db739a7
saroyar <yarsaro2001@gmail.com>
2024-03-05 16:26:13 +0530

    populate method is add

--- userFunction/userAction.js
+++ userFunction/userAction.js
@@ -1,25 +1,23 @@
 const User = require("../model/userModel");
-const Post=require("../model/postModel");
+const Post = require("../model/postModel");
 const jwt = require("jsonwebtoken");
-const LocationDB=require('../model/location');
-const cloudinary=require('cloudinary');
+const LocationDB = require("../model/location");
+const cloudinary = require("cloudinary");
 const postModel = require("../model/postModel");
 const { findDistance } = require("../location/locationActions");
 
 // register user
 exports.register = async (req, res) => {
   try {
+    const { name, email, password, location, image } = req.body;
 
-    const { name, email, password,location,image } = req.body;
-
-    if (!name || !email || !password||!location||!image) {
+    if (!name || !email || !password || !location || !image) {
       return res.status(400).json({
         success: false,
         message: "provide all document",
-        data:null
+        data: null,
       });
     }
-
 
     let lgUser;
     if (User.length > 0) {
@@ -30,7 +28,7 @@ exports.register = async (req, res) => {
       return res.status(400).json({
         success: false,
         message: "user already register",
-        data:null
+        data: null,
       });
     }
 
@@ -39,28 +37,23 @@ exports.register = async (req, res) => {
       folder: "theNewsUsers",
     });
 
-
-    const user = await User.create({ 
-      name, 
-      email, 
+    const user = await User.create({
+      name,
+      email,
       password,
       location,
-      image:{
-        public_id: myCloud.public_id, 
+      image: {
+        public_id: myCloud.public_id,
         url: myCloud.secure_url,
-      }
-
+      },
     });
 
-    const LocalUser=await LocationDB.findOne({name:location.name});
-    if(!LocalUser)
-    {
-      const newLocation=await LocationDB.create({location});
+    const LocalUser = await LocationDB.findOne({ name: location.name });
+    if (!LocalUser) {
+      const newLocation = await LocationDB.create({ location });
       newLocation.localUsers.push(user);
       await newLocation.save();
-    }
-    else
-    {
+    } else {
       LocalUser.localUsers.push(user);
       await LocalUser.save();
     }
@@ -70,19 +63,18 @@ exports.register = async (req, res) => {
 
     await user.save();
 
-
     res.status(201).json({
       success: true,
-      message:"User Register Success fully",
-      data:{
-        token:token
-      }
+      message: "User Register Success fully",
+      data: {
+        token: token,
+      },
     });
   } catch (error) {
     res.status(200).json({
       success: false,
       message: error.message,
-      data:null
+      data: null,
     });
   }
 };
@@ -91,18 +83,18 @@ exports.register = async (req, res) => {
 
 exports.getAllUser = async (req, res) => {
   try {
-    const userData = await User.findOne({email:req.user.email}).populate({
-      path:"posts",
-      populate:{
-        path:"comments.commented_user",
-        model:"users"
-      }
+    const userData = await User.findOne({ email: req.user.email }).populate({
+      path: "posts",
+      populate: {
+        path: "comments.commented_user",
+        model: "users",
+      },
     });
 
     res.status(200).json({
       success: true,
-      message:"All data of user are send",
-      data:userData
+      message: "All data of user are send",
+      data: userData,
     });
   } catch (error) {
     res.status(400).json({
@@ -120,7 +112,7 @@ exports.userLogin = async (req, res) => {
       return res.status(400).json({
         success: false,
         message: "provide all data",
-        data:null
+        data: null,
       });
     }
 
@@ -130,124 +122,114 @@ exports.userLogin = async (req, res) => {
       return res.status(200).json({
         success: false,
         message: "register first",
-        data:null
+        data: null,
       });
     }
 
     const token = jwt.sign({ email: email }, process.env.jwtPrivateKey);
 
-    res
-      .status(200)
-      .json({
-        success:true,
-        message:"User Login Success Fully",
-        data:{
-          token:token
-        }
-      });
+    res.status(200).json({
+      success: true,
+      message: "User Login Success Fully",
+      data: {
+        token: token,
+      },
+    });
   } catch (error) {
     res.status(200).json({
       success: false,
       message: error.message,
-      data:null
+      data: null,
     });
   }
 };
 
 // delete user
-exports.deleteUser=async(req,res)=>{
+exports.deleteUser = async (req, res) => {
   try {
-    
     // delete all the post of the use
 
-    req.user.posts.map(async(postId,index)=>{
-      const postOfUser=await Post.findOne({_id:postId});
-      
-      if(postOfUser)
-       await postOfUser.remove();
-    })
+    req.user.posts.map(async (postId, index) => {
+      const postOfUser = await Post.findOne({ _id: postId });
+
+      if (postOfUser) await postOfUser.remove();
+    });
 
     // delete userId from like array of post that like by user
-    req.user.likedPosts.map(async(postId,index)=>{
-      const likedPost=await Post.findOne({_id:postId});
-      if(likedPost)
-      {
-        let index=-1;
-        index=likedPost.likes.indexOf(req.user._id);
-        if(index>=0)
-        {
-          likedPost.likes.splice(index,1);
+    req.user.likedPosts.map(async (postId, index) => {
+      const likedPost = await Post.findOne({ _id: postId });
+      if (likedPost) {
+        let index = -1;
+        index = likedPost.likes.indexOf(req.user._id);
+        if (index >= 0) {
+          likedPost.likes.splice(index, 1);
           await likedPost.save();
         }
       }
-    })
+    });
 
     await req.user.remove();
 
     res.status(200).json({
-      success:true,
-      message:"your account is deleted",
-      data:null
-    })
-
-
+      success: true,
+      message: "your account is deleted",
+      data: null,
+    });
   } catch (error) {
     res.status(200).json({
       success: false,
       message: error.message,
-      data:null
+      data: null,
     });
   }
-}
+};
 
 // logout user
 
-exports.logout=async(req,res)=>{
+exports.logout = async (req, res) => {
   try {
-    
-     res.status(200).json({
-      success:true,
-      message:"you are logout",
-      data:{
-        id:req.user._id
-      }
-    })
-
-
-
+    res.status(200).json({
+      success: true,
+      message: "you are logout",
+      data: {
+        id: req.user._id,
+      },
+    });
   } catch (error) {
     res.status(200).json({
       success: false,
       message: error.message,
-      data:null
+      data: null,
     });
   }
-}
-
-
+};
 
 // find the posts which are near to user
-exports.post_Near_to_User=async(req,res)=>{
+exports.post_Near_to_User = async (req, res) => {
   try {
-    const userLocation=req.user.location.coordinates;
-    const distance=parseInt(req.query.distance)||100;
+    const userLocation = req.user.location.coordinates;
+    const distance = parseInt(req.query.distance) || 100;
 
-    const allPosts=await postModel.find();
-    const userNearPosts=allPosts.filter((posts)=>{
-      return findDistance(userLocation,posts.location.coordinates)<=distance;
-    })
+    const allPosts = await postModel.find().populate({
+      path: "posts",
+      populate: {
+        path: "comments.commented_user",
+        model: "users",
+      },
+    });
+    const userNearPosts = allPosts.filter((posts) => {
+      return findDistance(userLocation, posts.location.coordinates) <= distance;
+    });
 
     res.status(200).json({
-      success:true,
-      data:userNearPosts
-    })
-    
+      success: true,
+      data: userNearPosts,
+    });
   } catch (error) {
     res.status(200).json({
-      success:false,
-      data:null,
-      message:error.message
-    })
+      success: false,
+      data: null,
+      message: error.message,
+    });
   }
-}
-
+};
